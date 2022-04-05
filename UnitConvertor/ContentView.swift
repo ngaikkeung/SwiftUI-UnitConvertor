d7e293a3825bf372b33f51cb6b953c0360102a3f
ngaikkeung <ngaikkeung@gmail.com>
2022-04-05 22:19:09 +0800

    Initial Commit

---
+++ UnitConvertor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConvertor
+//
+//  Created by KK NGAI on 5/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnitConvertor/UnitConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertorApp.swift
+//  UnitConvertor
+//
+//  Created by KK NGAI on 5/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
